d9cb51e46c5cb41c4832d748f1f0db9e6a428069
runokami <98051960+runokami@users.noreply.github.com>
2025-03-18 18:28:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,16 @@ import os
 # Token'ı güvenli bir şekilde almak için os modülünü kullanıyoruz
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-# Client'ı oluşturuyoruz
-client = discord.Client()
+# Intents nesnesini oluşturuyoruz
+intents = discord.Intents.default()  # Varsayılan intents
+intents.message_content = True  # Mesaj içeriğini alabilmek için gerekli izin
+
+# Client'ı oluşturuyoruz ve intents'i geçiriyoruz
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} olarak giriş yapıldı!')
 
-# Botu çalıştırmak için token kullanıyoruz.
+# Botu çalıştırmak için token kullanıyoruz
 client.run(TOKEN)
