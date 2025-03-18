8b87d9cb085d092232857126bd47f012fcc199da
runokami <98051960+runokami@users.noreply.github.com>
2025-03-18 18:19:28 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+import os
+
+# Token'ı güvenli bir şekilde almak için os modülünü kullanıyoruz
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+# Client'ı oluşturuyoruz
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yapıldı!')
+
+# Botu çalıştırmak için token kullanıyoruz
+client.run(TOKEN)
