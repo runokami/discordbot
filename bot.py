d27a68b2e7695ffea2c77f2a8b643f249cb9e1bd
runokami <98051960+runokami@users.noreply.github.com>
2025-03-19 17:48:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import json
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 import io
 import requests
 from io import BytesIO
